3dc20cc4fd06876407393d7c49f13c7cf87bfe8e
sebastian medina <sebastianlopez_21@hotmail.com>
2022-08-24 10:34:37 -0400

    commit tree 7a7b083f306db54ea973f85184e6510ff266f359

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+
+function superbowlWin(array){
+     
+    let object = array.find(element => element.result === 'W');
+    console.log(object);
+    if(object){
+        return object.year;
+    }
+}
+
+
+
+
